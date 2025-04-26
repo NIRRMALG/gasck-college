40c1ffdbda0a1ab2b1b679fd4587af4574587a52
NIRRMALG <nirrmal.mis@gmail.com>
2025-04-26 12:44:07 +0530

    Update college.py

--- college.py
+++ college.py
@@ -7,6 +7,27 @@ app = Flask(__name__)
 def home():
     return render_template('homepage.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/courses')
+def courses():
+    return render_template('courses.html')
+
+@app.route('/faculty')
+def faculty():
+    return render_template('faculty.html')
+
+@app.route('/academics')
+def academics():
+    return render_template('academics.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
