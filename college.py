42cb5ba7e14d667deada174feef98dcf8252d30e
NIRRMALG <nirrmal.mis@gmail.com>
2025-04-26 11:14:19 +0530

    college.py

--- college.py
+++ college.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+import os
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("homepage.html")
+    return "Hello from Flask on Render!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use the PORT environment variable
+    app.run(host="0.0.0.0", port=port)         # Bind to 0.0.0.0
